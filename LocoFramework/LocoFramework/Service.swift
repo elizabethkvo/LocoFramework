2c733e18e2452fdd5d1906d9eaf0a5ba74919003
Elezabeth <Elezabeth@Elezabeths-MacBook-Pro.local>
2019-04-17 14:20:03 +0530

    finished

---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by Elezabeth on 17/04/19.
+//  Copyright © 2019 Elezabeth. All rights reserved.
+//
+
+import Foundation
+
+public class service{
+    init() {
+        
+    }
+    
+    static func doSomething() -> String{
+        
+        return "custom cocoapod"
+    }
+}
